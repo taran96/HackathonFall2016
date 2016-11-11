7003a06f1775082eb217734b73add8416217a8a1
Taranvir Waraich <taranvir@me.com>
2016-11-11 04:22:51 -0500

    Fixed indentation

--- RideBnB/parser.py
+++ RideBnB/parser.py
@@ -10,30 +10,6 @@ django.setup()
 from airbnb.models import Room
 csvFiles = glob.glob("csvs/*.csv")
 
-
-for i in csvFiles:
-    columns = {}
-    reader = csv.reader(open(i, "r"))
-    headers = next(reader, None)
-    for j in headers:
-        columns[j] = []
-    #print(list(reader)[0])
-    for row in reader:
-
-        for h, v in zip(headers,row):
-
-            columns[h].append(v)
-
-needed_data = {}
-needed_data['lon'] = list(map(float, columns["longitude"]))
-needed_data['lat'] = list(map(float, columns["latitude"]))
-needed_data['p_type'] = columns["property_type"]
-needed_data['air_id'] = list(map(int, columns["id"]))
-needed_data['price'] = list(map(lambda x: float(x[1:].replace(",","")),columns["price"]))
-needed_data['desc'] = columns["summary"]
-needed_data['title'] = columns["name"]
-needed_data['pic_url'] = columns["picture_url"]
-
 def get_amenities(lst):
     nested_list = []
 
@@ -48,28 +24,53 @@ def get_amenities(lst):
         nested_list.append(new_list)
     return nested_list
 
-amenities = get_amenities(list(columns['amenities']))
-needed_data["tv"] = []
-needed_data["wifi"] = []
-needed_data["ac"] = []
-needed_data["kitchen"] = []
-needed_data["heating"] = []
-for i in amenities:
-    needed_data["tv"].append(i[0])
-    needed_data["wifi"].append(i[1])
-    needed_data["ac"].append(i[2])
-    needed_data["kitchen"].append(i[3])
-    needed_data["heating"].append(i[4])
+for i in csvFiles:
+    columns = {}
+    reader = csv.reader(open(i, "r"))
+    headers = next(reader, None)
+    for j in headers:
+        columns[j] = []
+    #print(list(reader)[0])
+    for row in reader:
 
-new_needs = needed_data.copy()
-for i in range(len(needed_data['lon'])):
-    kwargs = {}
-    for key, val in new_needs.items():
-        print(key)
-        kwargs[key] = new_needs[key][i]
+        for h, v in zip(headers,row):
 
-    r = Room(**kwargs)
-    r.save()
+            columns[h].append(v)
+
+    needed_data = {}
+    needed_data['lon'] = list(map(float, columns["longitude"]))
+    needed_data['lat'] = list(map(float, columns["latitude"]))
+    needed_data['p_type'] = columns["property_type"]
+    needed_data['air_id'] = list(map(int, columns["id"]))
+    needed_data['price'] = list(map(lambda x: float(x[1:].replace(",","")),columns["price"]))
+    needed_data['desc'] = columns["summary"]
+    needed_data['title'] = columns["name"]
+    needed_data['pic_url'] = columns["picture_url"]
+
+
+
+    amenities = get_amenities(list(columns['amenities']))
+    needed_data["tv"] = []
+    needed_data["wifi"] = []
+    needed_data["ac"] = []
+    needed_data["kitchen"] = []
+    needed_data["heating"] = []
+    for i in amenities:
+        needed_data["tv"].append(i[0])
+        needed_data["wifi"].append(i[1])
+        needed_data["ac"].append(i[2])
+        needed_data["kitchen"].append(i[3])
+        needed_data["heating"].append(i[4])
+
+    new_needs = needed_data.copy()
+    for i in range(len(needed_data['lon'])):
+        kwargs = {}
+        for key, val in new_needs.items():
+            print(key)
+            kwargs[key] = new_needs[key][i]
+
+        r = Room(**kwargs)
+        r.save()
 
 '''
 for i in csvFiles:
