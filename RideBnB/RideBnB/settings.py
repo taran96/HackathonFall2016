9a7f14bc6ebfc1c3ed2e999811f96e3acdd292df
Taranvir Waraich <taranvir@me.com>
2016-11-20 23:17:03 -0500

    added allowed host

--- RideBnB/RideBnB/settings.py
+++ RideBnB/RideBnB/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".ridebnb.herokuapp.com"]
 
 
 # Application definition
